db943e367ed67435b742954d357cb25b9cdc1451
arun <arunkumarreddyc2@gmail.com>
2022-01-19 21:40:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const countdown = document.querySelector(".countdown");
+
+countdown.innerText = "10";
+setTimeout(() => {
+  countdown.innerText = "9";
+  setTimeout(() => {
+    countdown.innerText = "8";
+    setTimeout(() => {
+      countdown.innerText = "7";
+      setTimeout(() => {
+        countdown.innerText = "6";
+        setTimeout(() => {
+          countdown.innerText = "5";
+          setTimeout(() => {
+            countdown.innerText = "4";
+            setTimeout(() => {
+              countdown.innerText = "3";
+              setTimeout(() => {
+                countdown.innerText = "2";
+                setTimeout(() => {
+                  countdown.innerText = "1";
+                  setTimeout(() => {
+                    countdown.innerText = "Happy Independence Day";
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
